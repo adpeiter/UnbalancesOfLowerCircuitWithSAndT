3c4a6e48b1f455df9b6e7790983fb1f2a4a72580
adpeiter <aristosdp@gmail.com>
2015-09-19 22:05:44 -0300

    alterado para java 7

--- src/balanced/disjoint/paths/BalancedDisjointPaths.java
+++ src/balanced/disjoint/paths/BalancedDisjointPaths.java
@@ -1,6 +1,7 @@
 package balanced.disjoint.paths;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
@@ -11,8 +12,9 @@ public class BalancedDisjointPaths {
 
     private Path path = new Path();
     
-/*  public ArrayList<Vertex> balancedDisjointPaths(Graph g, Vertex s, Vertex t) {
-        
+    public ArrayList<Vertex> balancedDisjointPaths(Graph g, Vertex s, Vertex t) {
+    
+        /*
         aqui faremos A função que resolve nosso problema:
         - listar todos os caminhos entre s e t, que utilizem os vértices dos caminhos encontrados
             pela função lowerCycleCommonVertices
@@ -21,33 +23,26 @@ public class BalancedDisjointPaths {
         - verificar se existem p1, p2 e p3 e p4 tais que p1 < p2 <= p3 < p4 e p1 + p4 = p2 + p3, que é o
             caso em que temos dois pares (um mais e outro menos balanceado)
             do contrário, p1 e p2 serão o par mais e menos balanceado
+        */
+        return null;
     }
-    */
+    
     
     // obtém o menor ciclo que contém s e t, dando preferência para ciclos não hamiltonianos
     public ArrayList<Vertex> lowerCycleCommonVertices(Graph g, Vertex s, Vertex t) {
         
-        ArrayList<Vertex> path1, path2;
+        ArrayList<Vertex> pairOfPaths;
         
         // encontra o menor caminho (se existir)
-        path1 = this.path.shortestPath(s, t);
+        pairOfPaths = this.path.shortestPath(s, t);
         
         // marca os vértices já usados para que tenham prioridade nas listas de adjacência
         for (Vertex v : g.vertices)
-            v.prefer = path1.indexOf(v) != -1;
-        
-        // comparator para ordenar as listas de adjacências pelo critério acima
-        // OBS: este tipo de expressão (lambda) só está disponível no Java 8
-        // mudei a versão do Java do projeto pois não estava conseguindo entender o Comparator das versões
-        // anteriores, que necessita precisa ser implementado de forma explícita
-        // se alguém quiser mudar isso, pode, contando que continue ordenando certo... hahaha!!!
-        Comparator<Vertex> comparator = (v, u) -> {
-            return Boolean.compare(u.prefer, v.prefer); 
-        };
+            v.prefer = pairOfPaths.indexOf(v) != -1;
         
         // remove as arestas do caminho mais curto´
-        for (int i = 0; i < path1.size() - 1; i++)
-            g.removeEdge(path1.get(i), path1.get(i + 1));
+        for (int i = 0; i < pairOfPaths.size() - 1; i++)
+            g.removeEdge(pairOfPaths.get(i), pairOfPaths.get(i + 1));
         
         System.out.print(g.toString());
         
@@ -57,16 +52,22 @@ public class BalancedDisjointPaths {
         // remover os pais para que não tenha perigo de utilizar algum pai setado na busca anterior
         // ao fazer a extração do caminho
         for (Vertex v : g.vertices) {
-            v.listOfAdjacency.sort(comparator);
+            // ordena as listas de adjacências pelo critério prefer (vértices já utilizados no caminho
+            // anterior vão para o início
+            Collections.sort(v.listOfAdjacency, new Comparator<Vertex>() {
+                @Override
+                public int compare(Vertex  a, Vertex b) {
+                    return Boolean.compare(b.prefer, a.prefer);
+                }
+            });
             v.stateColor = StateColor.White;
             v.parent = null;
         }
         
         // encontra o segundo menor caminho (se existir)
-        path2 = this.path.shortestPath(s, t);
-        path1.addAll(path2);
+        pairOfPaths.addAll(this.path.shortestPath(s, t));
         
-        return path1;
+        return pairOfPaths;
         
     }
     
